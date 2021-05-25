a90ef2615b3f04bc37456f5d71e6bce129d5eee2
Andreea Druga <andreea.druga51@gmail.com>
2021-05-25 18:23:31 -0500

    things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,13 @@
 public class BranchingPractice {
+
     public static void main(String[] args) {
+
+        // created new project
+        // enable vcs
+        //added git ignore with .idea, target folder, .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
 
     }
 }
