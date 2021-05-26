3c5ac383066dee382aed93cb7303a37b33298afe
Andreea Druga <andreea.druga51@gmail.com>
2021-05-25 20:18:24 -0500

    tc 001 has been completed

--- src/test/java/TestCase001.java
+++ src/test/java/TestCase001.java
@@ -3,6 +3,9 @@ public class TestCase001 {
 
         //work1
         //work2
+        //finished this test case , ready to push to remote
+        //ready to open pull request (permission to merge)
+        //discuss and eventually merge the code
 
 
     }
